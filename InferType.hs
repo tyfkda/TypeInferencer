2c56399dee5805d8cfa905dbf9f52bfa49eafcb5
tyfkda <tyfkda@gmail.com>
2015-11-14 09:58:38 +0900

    Create AST

--- InferType.hs
+++ InferType.hs
@@ -3,6 +3,10 @@ import Text.ParserCombinators.Parsec.Expr
 import Text.ParserCombinators.Parsec.Language
 import qualified Text.ParserCombinators.Parsec.Token as P
 
+data Expr = Natural Integer
+          | BinOp String Expr Expr
+  deriving (Show)
+
 lexer :: P.TokenParser ()
 lexer = P.makeTokenParser (haskellDef { reservedOpNames = ["*", "/", "+", "-"] })
 
@@ -10,26 +14,26 @@ natural     = P.natural lexer
 parens      = P.parens lexer
 reservedOp  = P.reservedOp lexer
 
-expr :: Parser Integer
+expr :: Parser Expr
 expr = buildExpressionParser table term <?> "expression"
   where
-    table = [[unary "-" negate],
-             [binop "*" (*) AssocLeft, binop "/" div AssocLeft],
-             [binop "+" (+) AssocLeft, binop "-" (-) AssocLeft]]
-    binop s op assoc = Infix (do{ reservedOp s; return op } <?> "operator") assoc
+    table = [[unary "-" (BinOp "-" (Natural 0))],
+             [binop "*" AssocLeft, binop "/" AssocLeft],
+             [binop "+" AssocLeft, binop "-" AssocLeft]]
+    binop op assoc = Infix (do{ reservedOp op; return (BinOp op) } <?> "operator") assoc
     unary s op = Prefix (do{ reservedOp s; return op })
 
-term :: Parser Integer
+term :: Parser Expr
 term =
   do {
     parens expr;
   } <|> do {
     n <- natural;
-    return n
+    return $ Natural n
   } <?>
     "term"
 
-stmt :: Parser Integer
+stmt :: Parser Expr
 stmt = do
   e <- expr
   eof
